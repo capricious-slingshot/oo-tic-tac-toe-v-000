713829124a8e94a8704ee42a640b8d8cf106a382
Carden Z <ephemere@protonmail.com>
2018-12-18 03:09:21 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,6 +16,7 @@ class TicTacToe
   end
 
 	def display_board
+		puts "-----------"
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
 		puts "--------"
 		puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
